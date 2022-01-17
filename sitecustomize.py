98acbb5f0e0eaf9481fd2b9d1bcb3970cb27efa4
Quinten <quinten.roets@gmail.com>
2022-01-17 20:32:12 +0100

    install displayhook for all scripts

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,3 +1,14 @@
+import sys
 import tbhandler
 
+
+def displayhook(value):
+    # only install after first display usage
+    # save startup time for scripts that dont use display
+    from rich import pretty
+    pretty.install()
+    sys.displayhook(value)
+
+
 tbhandler.install()
+sys.displayhook = displayhook
