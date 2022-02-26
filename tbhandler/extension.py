3c621c86e8f96ddf196b7f5c2c88430460919a36
Quinten <quinten.roets@gmail.com>
2022-02-26 14:56:59 +0100

    format file

--- tbhandler/extension.py
+++ tbhandler/extension.py
@@ -1,10 +1,9 @@
 def load_ipython_extension(ip):
     from rich import pretty
-    pretty.install()
-    
-    # first install traceback monkeypatch to make the traceback work in notebooks as well
     from . import monkeypatch
-    monkeypatch.install()
-    
     from rich.traceback import install
+
+    pretty.install()
+    # first install traceback monkeypatch to make the traceback work in notebooks as well
+    monkeypatch.install()
     install(show_locals=True)
