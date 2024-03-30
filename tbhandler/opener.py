9697c561da04db13ce9f1ab4bc436d1e2ab74fde
Quinten <quinten.roets@gmail.com>
2024-03-30 11:17:19 -0700

    migrate to powercli

--- tbhandler/opener.py
+++ tbhandler/opener.py
@@ -7,7 +7,9 @@ def main():
     path = sys.argv[1]
     open_file = cli.confirm("Open file?", default=False)
     if open_file:
-        pycharm_is_open = cli.is_success("xdotool search --onlyvisible pycharm")
+        pycharm_is_open = cli.completes_successfully(
+            "xdotool search --onlyvisible pycharm"
+        )
         opener = "pycharm-professional" if pycharm_is_open else "kate"
         cli.run(opener, path, wait=False)
     sys.exit(open_file)
