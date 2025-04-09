dff41ad5502a11be48aaded6d7f68afe9c83fe2f
Quinten <quinten.roets@gmail.com>
2025-04-09 03:54:29 +0000

    pre-commit auto updates

--- src/powertrace/models/path.py
+++ src/powertrace/models/path.py
@@ -2,6 +2,7 @@ from typing import TypeVar, cast
 
 import superpathlib
 from simple_classproperty import classproperty
+from typing_extensions import Self
 
 T = TypeVar("T", bound="Path")
 
@@ -9,6 +10,7 @@ T = TypeVar("T", bound="Path")
 class Path(superpathlib.Path):
     @classmethod
     @classproperty
+<<<<<<< HEAD
     def logs(cls: type[T]) -> T:
         path = cls.script_assets / ".tracebacks"
         return cast(T, path)
@@ -28,3 +30,19 @@ class Path(superpathlib.Path):
     def short_log(cls: type[T]) -> T:
         path = cls.logs / "short_traceback.txt"
         return cast(T, path)
+=======
+    def source_root(cls) -> Self:
+        return cls(__file__).parent.parent
+
+    @classmethod
+    @classproperty
+    def assets(cls) -> Self:
+        path = cls.script_assets / cls.source_root.name
+        return cast("Self", path)
+
+    @classmethod
+    @classproperty
+    def config(cls) -> Self:
+        path = cls.assets / "config" / "config.yaml"
+        return cast("Self", path)
+>>>>>>> template
